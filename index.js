87befa202540daf6579b610988868ab73e94cf03
{Jeon <jsy020222@gmail.com>
2022-03-20 23:46:25 +0900

    검색창 추가, 반응형 추가

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const btn = document.querySelector('.menu-bar-btm')
 const menu = document.querySelector('.menu-bar')
-const search = document.querySelector('.search')
+const search = document.querySelector('.search-box')
 
 btn.addEventListener('click', () => {
     menu.classList.toggle('active');
