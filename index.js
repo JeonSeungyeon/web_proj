33a4140f031c198b9823fc66eb33080617625dd5
{Jeon <jsy020222@gmail.com>
2022-03-24 18:19:57 +0900

    언어 변경 추가, 테마 변경 추가, 검색창 추가

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// 메뉴바 자바스크립트
 const btn = document.querySelector('.menu-bar-btm')
 const menu = document.querySelector('.menu-bar')
 const search = document.querySelector('.search-box')
@@ -7,4 +8,9 @@ btn.addEventListener('click', () => {
     search.classList.toggle('active');
 });
 
+    // 어두운 테마 자바스크립트
+const checkbox = document.getElementById('checkbox')
+checkbox.addEventListener('change',() => {
+    document.body.classList.toggle('dark')
+});
 
